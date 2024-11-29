4c4a7a5fb5ef85918893240c53ab4d1470a23f70
Elias Gabriel <elias.gabrielfn12@gmail.com>
2024-11-29 16:26:35 -0400

    feat: Upload project

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -6,19 +6,76 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct Todo: Identifiable, Equatable, Codable {
+  var id = UUID()
+  var text: String = ""
+  var isDone = false
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+  @State private var todos: [Todo] = []
+
+  private let key = "todosModelViewData"
+
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach($todos) { $todo in
+          HStack {
+            TextField("Todo", text: $todo.text)
+            Spacer()
+            Image(systemName: todo.isDone ? "checkmark.circle" : "circle")
+              .onTapGesture {
+                todo.isDone.toggle()
+              }
+          }
+          .onChange(of: todo) { oldValue, newValue in
+            save(items: todos, key: key)
+          }
+
         }
-        .padding()
+      }
+      .onAppear {
+        todos = load(key: key)
+      }
+      .navigationTitle("Todos ✅")
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button {
+            todos.append(Todo())
+            save(items: todos, key: key)
+          } label: {
+            Image(systemName: "plus")
+          }
+
+        }
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
+}
+
+func save<T: Identifiable & Codable>(items: [T], key: String) {
+  if let encoded = try? JSONEncoder().encode(items) {
+    UserDefaults.standard.setValue(encoded, forKey: key)
+  }
+}
+
+
+func load<T: Identifiable & Codable>(key: String) -> [T] {
+  guard let data = UserDefaults.standard.data(forKey: key) else {
+    return []
+  }
+
+  if let dataArray = try? JSONDecoder().decode([T].self, from: data) {
+    return dataArray
+  }
+
+  return []
 }
